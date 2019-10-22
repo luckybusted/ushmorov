b2f1f3b0f6166e07777d021b90b3314632948bd7
Ivan Ushmorov <ivan.ushmorov@raysono.com>
2019-10-22 14:35:43 +0200

    added the google analytics module

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
-import pkg from './package'
+import pkg from './package';
 
 export default {
   mode: 'spa',
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
     meta: [
@@ -13,36 +13,37 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
 
   /*
-  ** Global CSS
-  */
-  css: [
-    '@/assets/styles/all.scss'
-  ],
+   ** Global CSS
+   */
+  css: ['@/assets/styles/all.scss'],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    [
+      '@nuxtjs/google-analytics',
+      {
+        id: 'UA-149932633-1'
+      }
+    ]
   ],
 
   vuetify: {
@@ -50,19 +51,19 @@ export default {
   },
 
   /*
-  ** Axios module configuration
-  */
+   ** Axios module configuration
+   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     /* extend(config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
@@ -75,4 +76,4 @@ export default {
       }
     } */
   }
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,78 @@
 <template>
-
-
   <v-layout wrap class="overview">
-    <AboutMe/>
+    <AboutMe />
     <v-layout wrap>
-
-
-      <v-flex xs12 sm6 pa-3 md4 v-for="(project) in projects" :key="project.id" v-if="project.active" class="project">
-
+      <v-flex
+        xs12
+        sm6
+        pa-3
+        md4
+        v-for="(project) in projects"
+        :key="project.id"
+        v-if="project.active"
+        class="project"
+      >
         <v-card height="100%">
           <!--<v-img :src="'http://dev.ushmorov.de/' + project.image.path"
-                 aspect-ratio="1"/>-->
-
+          aspect-ratio="1"/>-->
 
           <v-card-title primary-title>
             <h3 class="md-title">{{ project.title }}</h3>
             <div class="mb-2" v-html="project.summary"></div>
             <ul class="technics">
-              <li v-for="(tag) in project.tags" :key="tag" class="mr-1">
-                #{{ tag }}
-              </li>
+              <li v-for="(tag) in project.tags" :key="tag" class="mr-1">#{{ tag }}</li>
             </ul>
           </v-card-title>
 
           <div class="text-xs-right">
             <v-btn flat color="success" :href="project.link">zum Projekt</v-btn>
           </div>
-
-
         </v-card>
-
       </v-flex>
     </v-layout>
   </v-layout>
 </template>
 
 <script>
-  import AboutMe from '~/components/AboutMe.vue'
-  import avatar from '~/assets/images/twitter-avatar.jpg'
+import AboutMe from "~/components/AboutMe.vue";
+import avatar from "~/assets/images/twitter-avatar.jpg";
 
-  export default {
-    components: {
-      AboutMe
-    },
-    data() {
-      return {
-        projects: []
-      }
-    },
-    head(){
-      return {
-        title: 'Homepage of Ivan Ushmorov',
-        meta: [
-          { hid: 'description', name: 'description', content: 'Frontend developer. Based in Munich, Germany.' },
-          { hid: 'twitter-card', name: 'twitter:card', content: 'summary' },
-          { hid: 'twitter-site', name: 'twitter:site', content: '@luckybusted' },
-          { hid: 'twitter-image', name: 'twitter:image', content: avatar }
-          ]
-      }
-    },
-    created() {
-      this.fetchProjects()
-    },
-    methods: {
-      async fetchProjects() {
-        await this.$axios.$get('https://dev.ushmorov.de/backend/api/collections/get/Projects')
-          .then((response) => {
-
-            console.log('PROJECTS', response.entries)
-
-            this.projects = response.entries
-          })
-      }
+export default {
+  components: {
+    AboutMe
+  },
+  data() {
+    return {
+      projects: []
+    };
+  },
+  head() {
+    return {
+      title: "Homepage of Ivan Ushmorov",
+      meta: [
+        {
+          name: "description",
+          content: "Frontend developer. Based in Munich, Germany."
+        },
+        { name: "twitter:card", content: "summary" },
+        { name: "twitter:site", content: "@luckybusted" },
+        { name: "twitter:image", content: avatar }
+      ]
+    };
+  },
+  created() {
+    this.fetchProjects();
+  },
+  methods: {
+    async fetchProjects() {
+      await this.$axios
+        .$get("https://dev.ushmorov.de/backend/api/collections/get/Projects")
+        .then(response => {
+          this.projects = response.entries;
+        });
     }
   }
+};
 </script>
 
 <style>
